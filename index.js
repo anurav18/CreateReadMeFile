98373baf27c6de3489be4375554b0d198210f00d
Anu Rav <anusharavva@gmail.com>
2020-10-28 17:08:14 -0700

    Inquirer Prompts

--- index.js
+++ index.js
@@ -21,17 +21,66 @@ inquirer.prompt([
     type:'input',
     name:'usage',
     message: 'Enter about usage'
+},
+{
+    type:'list',
+    name:'license',
+    message: 'Enter about usage',
+    choices: ["MIT","BSD","GPL"]
+},
+{
+    type:'input',
+    name:'contributing',
+    message: 'Enter about usage'
+}
+,
+{
+    type:'input',
+    name:'testInstructions',
+    message: 'Enter about usage'
+},
+{
+    type:'input',
+    name:'github_username',
+    message: 'Enter your github user name'
+},
+{
+    type:'input',
+    name:'email',
+    message: 'Enter your email'
+},
+{
+    type:'input',
+    name:'github_repository',
+    message: 'Enter the link to the github profile'
+},
+{
+    type:'input',
+    name:'project_page',
+    message: 'Enter the link to the github profile'
 }
 ]).then(function(response){
 
     var filename = "README.md";
     var filecontent = `
     
-    ### ${response.title}
+    # ${response.title}
+
+
+
+    ##Tests
+    ${response.testInstructions}
 
     ## Installation
     ${response.installation}
+
+    ## License
+    Licensed under the ${response.license} license.
     
+    ## Questions
+    * Feel free to reach me with additional questions using my email : ${response.email}
+    * Please follow me on github for more intresting updates in my [repository](${response.github_repository})
+    * Explore the [project page](${response.project_page})
     
     `
     fs.writeFile(filename,filecontent,function(err){
